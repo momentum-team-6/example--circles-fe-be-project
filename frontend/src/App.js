f992cc45f40311087bdf7ee4eecd39916cb7e4ca
Clinton Dreisbach <clinton@dreisbach.us>
2021-02-01 14:36:33 -0500

    Remove unneeded boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import 'tachyons'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App sans-serif mw8 center mv3 ph2'>
+
+      <header className='NavBar'>
+        <div className='pv2 flex items-center'>
+          <div className='flex flex-auto items-center justify-between'>
+            <h1 className='f2 ma0 pa0 lh-solid'>
+              <span className='no-underline black'>Circles</span>
+            </h1>
+          </div>
+        </div>
+      </header>
+
+      <div class='pv2'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
-      </header>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

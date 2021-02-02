4109b5e765fb433bcbd4e76746ba728c9baa6260
Clinton Dreisbach <clinton@dreisbach.us>
2021-02-02 11:00:54 -0500

    Add models for Circles app

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     pass
+
+
+class Circle(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(to=User,
+                                     through='CircleMembership',
+                                     related_name="circles")
+
+
+class CircleMembership(models.Model):
+    circle = models.ForeignKey(to=Circle,
+                               related_name="memberships",
+                               on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User,
+                             related_name="memberships",
+                             on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(to=User,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+    circle = models.ForeignKey(to=Circle,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
